e2fc86fa1799339878f28aa62a94fd694c600bbc
Naeleno <naeleno@gmail.com>
2018-01-15 13:08:27 +0300

    add croller seekbar there is problem in setProgress

--- app/src/main/java/com/eno/nael/cooktimer/MainActivity.java
+++ app/src/main/java/com/eno/nael/cooktimer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.eno.nael.cooktimer;
 
+
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
@@ -7,51 +9,50 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SeekBar;
 import android.widget.TextView;
-
-import com.akaita.android.circularseekbar.CircularSeekBar;
-
+import com.sdsmdg.harjot.crollerTest.Croller;
+import com.sdsmdg.harjot.crollerTest.OnCrollerChangeListener;
 public class MainActivity extends AppCompatActivity {
 
-    CircularSeekBar seekBar;
+    // Declaring
+    Croller croller;
     Button button;
     TextView textView;
     Boolean counterIsActive=false;
     CountDownTimer countDownTimer;
     MediaPlayer mediaPlayer;
     TextView textView2;
+
+
+
+// reset method
 public void reset (){
     textView.setText("5:00");
-    seekBar.setProgress(300);
+   croller.setProgress(300);
     counterIsActive=false;
-    seekBar.setEnabled(true);
+ //   croller.setEnabled(true);
     countDownTimer.cancel();
     button.setText("Go!");
     textView2.setVisibility(View.INVISIBLE);
-
-
-
 }
 
-    public void Any (int leftTime){
+
+public void Any (int leftTime){
         int mint =(int) leftTime/60;
         int secnd =leftTime-mint*60;
         String StringSecnd=Integer.toString(secnd);
         if (secnd<=9){
             StringSecnd="0"+Integer.toString(secnd);
         }
-
         textView.setText(Integer.toString(mint)+":"+StringSecnd);
-    }
-
-    public void timerControl (View view){
+}
+public void timerControl (View view){
         if(counterIsActive==false) {
             counterIsActive = true;
-            seekBar.setEnabled(false);
+            //croller.setEnabled(false);
             button.setText("Stop");
             textView2.setVisibility(View.INVISIBLE);
-            countDownTimer=new  CountDownTimer((int) seekBar.getProgress()* 1000 + 100 , 1000) {
+            countDownTimer = new CountDownTimer( 100*1000 + 100 , 1000) {
 
                 @Override
                 public void onTick(long millisUntilFinished) {
@@ -77,33 +78,55 @@ public void reset (){
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CircularSeekBar seekBar=(CircularSeekBar)findViewById(R.id.seekbar);
+
         button=(Button) findViewById(R.id.button);
         textView=(TextView) findViewById(R.id.textView);
         textView2=(TextView) findViewById(R.id.textView2);
 
-        seekBar.setMax(900*2);
-        seekBar.setProgress(300);
-        seekBar.setOnCircularSeekBarChangeListener(new CircularSeekBar.OnCircularSeekBarChangeListener() {
+        Croller croller = (Croller) findViewById(R.id.croller);
+        croller.setIndicatorWidth(10);
+        croller.setBackCircleColor(Color.parseColor("#EDEDED"));
+        croller.setMainCircleColor(Color.WHITE);
+        croller.setMax(900*2);
+        croller.setStartOffset(45);
+        croller.setIsContinuous(false);
+        croller.setLabelColor(Color.BLACK);
+        croller.setProgressPrimaryColor(Color.parseColor("#0B3C49"));
+        croller.setIndicatorColor(Color.parseColor("#0B3C49"));
+        croller.setProgressSecondaryColor(Color.parseColor("#EEEEEE"));
+        croller.setProgress(300);
+
+        Log.i("theis is the taaaaaaag", String.valueOf(croller.getProgress()));
+
+
+
+
+
+
+       // croller = (Croller) findViewById(R.id.croller);
+
+        croller.setOnProgressChangedListener(new Croller.onProgressChangedListener() {
             @Override
-            public void onProgressChanged(CircularSeekBar seekBar, float progress, boolean fromUser) {
-               Any((int) progress);
-            }
-
-            @Override
-            public void onStartTrackingTouch(CircularSeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(CircularSeekBar seekBar) {
-
+            public void onProgressChanged(int progress) {
+                Any(( progress));
             }
         });
-        seekBar.setOnCenterClickedListener(new CircularSeekBar.OnCenterClickedListener() {
+
+         croller.setOnCrollerChangeListener(new OnCrollerChangeListener() {
             @Override
-            public void onCenterClicked(CircularSeekBar seekBar, float progress) {
-                seekBar.setProgress(0);
+            public void onProgressChanged(Croller croller, int progress) {
+               // use the progress
+                Log.i("theis is the nowwwwwww", String.valueOf(croller.getProgress()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(Croller croller) {
+                // tracking started
+            }
+
+            @Override
+            public void onStopTrackingTouch(Croller croller) {
+               // croller.setProgress(0); // tracking stopped
             }
         });
 
---
+++ app/src/main/java/com/eno/nael/cooktimer/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.eno.nael.cooktimer;
+
+/**
+ * Created by MAXIMILIAN on 1/13/2018.
+ */
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+}
